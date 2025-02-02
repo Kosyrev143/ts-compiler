9dc80329d6849426525f635d2c0f9cbd3e4810bf
Kosyrev143 <artemkosyrev93@gmail.com>
2025-02-02 13:10:42 +0500

    feat: (ts-compiler) add compiler

--- ts-compiler.js
+++ ts-compiler.js
@@ -1,4 +1,91 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const ts = require('typescript');
 
-const formatHost = {};
+const formatHost = {
+  getCanonicalFileName: (path) => path,
+  getCurrentDirectory: ts.sys.getCurrentDirectory,
+  getNewLine: () => ts.sys.newLine,
+};
+
+function getTSConfig() {
+  const configPath = ts.findConfigFile(
+    './',
+    ts.sys.fileExists,
+    'tsconfig.json',
+  );
+  const readConfigFileResult = ts.readConfigFile(configPath, ts.sys.readFile);
+  if (readConfigFileResult.error) {
+    throw new Error(
+      ts.formatDiagnostic(readConfigFileResult.error, formatHost),
+    );
+  }
+  const jsonConfig = readConfigFileResult.config;
+  const convertResult = ts.convertCompilerOptionsFromJson(
+    jsonConfig.compilerOptions,
+    './',
+  );
+  if (convertResult.errors && convertResult.errors.length > 0) {
+    throw new Error(ts.formatDiagnostic(convertResult.errors, formatHost));
+  }
+  const compilerOptions = convertResult.options;
+  return compilerOptions;
+}
+
+function displayFileName(originalFunc, operationName) {
+  let displayEnabled = false;
+  function displayFunction() {
+    const fileName = arguments[0];
+    if (displayEnabled) {
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+      process.stdout.write(`${operationName}: ${fileName}`);
+    }
+    return originalFunc(...arguments);
+  }
+  displayFunction.originalFunc = originalFunc;
+  displayFunction.enableDisplay = () => {
+    if (process.stdout.isTTY) {
+      displayEnabled = true;
+    }
+  };
+  displayFunction.disableDisplay = () => {
+    if (displayEnabled) {
+      displayEnabled = false;
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+    }
+  };
+  return displayFunction;
+}
+
+function compile() {
+  const compilerOptions = getTSConfig();
+  const compilerHost = ts.createCompilerHost(compilerOptions);
+  compilerHost.readFile = displayFileName(compilerHost.readFile, 'Reading');
+  compilerHost.readFile.enableDisplay();
+  const program = ts.createProgram(
+    ['./src/main.ts'],
+    compilerOptions,
+    compilerHost,
+  );
+  compilerHost.readFile.disableDisplay();
+
+  console.log(
+    ts.formatDiagnosticsWithColorAndContext(
+      ts.getPreEmitDiagnostics(program),
+      formatHost,
+    ),
+  );
+
+  compilerHost.writeFile = displayFileName(compilerHost.writeFile, 'Emitting');
+  compilerHost.writeFile.enableDisplay();
+  const emitResult = program.emit();
+  compilerHost.writeFile.disableDisplay();
+  console.log(
+    ts.formatDiagnosticsWithColorAndContext(emitResult.diagnostics, formatHost),
+  );
+  return emitResult.diagnostics.length === 0;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions,@typescript-eslint/no-require-imports
+compile() && require('./dist/main.js');
